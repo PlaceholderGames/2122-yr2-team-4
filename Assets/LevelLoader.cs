203da67aa086231542a016e2ae3fa6432f95409f
nimshi89 <krajcsirik20@gmail.com>
2021-10-10 10:55:25 +0100

    Added an automated level loader with an animation. NOT FINAL VERSION

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator Transition;
+
+    public float LoadingTime = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Backspace))  //Current loading triger 
+        {
+
+            LoadNextLevel();
+        
+        }
+    }
+    public void LoadNextLevel() //this weill call in the Unity level manager and load the next scene
+    {
+
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));       //The order of the levels can be set in the project build
+
+    }
+
+    IEnumerator LoadLevel(int levelIndex) {     //loading in the animation for the level transition
+
+        Transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(LoadingTime);
+
+        SceneManager.LoadScene(levelIndex);
+    
+    }
+}
