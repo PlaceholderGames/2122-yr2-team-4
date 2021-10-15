d167e6110efa52cd9389731afc9f5265522b32da
nimshi89 <krajcsirik20@gmail.com>
2021-10-15 15:26:29 +0100

    added some basic AI testing files

---
+++ Assets/BaseAI.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BaseAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+
+
+    public Animator anim;
+
+    public float maxHetalth;
+    public float currentHealth;
+    public float speed = 4f;
+
+
+    void Start()
+    {
+        currentHealth = maxHetalth;
+
+        anim = gameObject.GetComponent<Animator>();
+        NavMeshAgent agent = GetComponent<NavMeshAgent>(); _
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {   
+
+        if (currentHealth == 0) {
+
+            anim.Play("Death");
+
+            //GameObject.Destroy(Boss);
+
+
+        }
+    }
+
+}
